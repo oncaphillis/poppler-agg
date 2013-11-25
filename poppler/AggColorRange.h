ba9e6c593a598a925ca0dfa4b68e8a381d932087
Sebastian Kloska <oncaphillis@snafu.de>
2013-11-25 15:29:20 +0100

    output operator for poppler/AggColorRange.h added

--- poppler/AggColorRange.h
+++ poppler/AggColorRange.h
@@ -25,6 +25,14 @@
 #include "AggColorTraits.h"
 
 #include <map>
+#include <iostream>
+
+/** @short Mediator between the AGG and the Poppler representation of color 
+    gradients. 
+
+    Central is the operator[] which allows us to access a linear index of 
+    AggColorRange::color_t values.
+*/
 
 template< class TRAITS,class GFXSHADING >
 struct AggColorRange
@@ -50,17 +58,13 @@ struct AggColorRange
     const color_t operator [] (unsigned i) const
     { 
         GfxColor gc;
-
         double d = _s.getDomain0() + ::fabs( ( _s.getDomain1() - _s.getDomain0()) * i ) / _size;
-
         _s.getColor(d,&gc);
-
         color_t c = traits_t::toAggColor(_s.getColorSpace(),&gc,c);
-        
         c.a = 1.0;
-        
         return c;
     }
+
 private:
     AggColorRange(const AggColorRange &);
     AggColorRange();
@@ -68,5 +72,10 @@ private:
     gfx_shading_t & _s;
     int             _size;
 };
+
+template<class T,class S>
+std::ostream & operator<<(std::ostream & os,const AggColorRange<T,S> & cr)  {
+    return os << "cr:@" << cr.size() << " (" << cr[0] << "..." << cr[cr.size()/2] << "..." << cr[cr.size()] << ")";
+}
 
 #endif // AGGCOLORRANGE_H
